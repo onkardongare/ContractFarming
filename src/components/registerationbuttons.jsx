d455524de75d0f895dbb02be2a7183ff25533689
Raj Rasane <rasaneraj04@gmail.com>
2024-09-01 12:26:25 +0530

    change route name for auth

--- src/components/registerationbuttons.jsx
+++ src/components/registerationbuttons.jsx
@@ -6,7 +6,7 @@ function RegistrationButtons() {
   return (
     <div class="flex justify-center items-center mt-12 space-x-8">
       
-      <Link to='/farmer-signup' className='btn btn-farmer bg-[#1f3b2f] text-white text-3xl font-medium py-3 px-16 rounded-full transition duration-300 ease-in-out'>
+      <Link to='/farmer-registration' className='btn btn-farmer bg-[#1f3b2f] text-white text-3xl font-medium py-3 px-16 rounded-full transition duration-300 ease-in-out'>
         <button >
           Register as a<br/> Farmer
         </button>
@@ -14,7 +14,7 @@ function RegistrationButtons() {
       
         <span class="text-2xl font-semibold text-gray-700 opacity-70">or &nbsp;</span>
 
-      <Link to='/buyer-signup' className='btn btn-farmer bg-[#1f3b2f] text-white text-3xl font-medium py-3 px-16 rounded-full transition duration-300 ease-in-out'>
+      <Link to='/buyer-registration' className='btn btn-farmer bg-[#1f3b2f] text-white text-3xl font-medium py-3 px-16 rounded-full transition duration-300 ease-in-out'>
         <button >
         Register as a <br/> Buyer/Business
         </button>
